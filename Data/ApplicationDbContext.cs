b88308591b97eb38f78eb99f0decbc3f0d21d3ae
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2023-12-03 10:40:39 -0500

    fix: double applicationdbcontext definitions

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,10 +5,11 @@ using Microsoft.EntityFrameworkCore;
 namespace PRPicks.Models
 {
    public class ApplicationDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
-  {
-     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-    }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
      // Change to be your model(s) and table(s)
      public DbSet<Collection> Collections { get; set; }
      public DbSet<Product> Products { get; set; }
